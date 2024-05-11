b4c13c7623d4570ccd6518324af226d1de84d4db
ghkgus407 <143527072+ghkgus407@users.noreply.github.com>
2024-05-11 21:32:28 +0900

    Title: 변수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; 
+    string b;
+
+    a=3;
+    b='C';
+
+    cout << a << "..." << b << endl;
+    return 0;
+}
