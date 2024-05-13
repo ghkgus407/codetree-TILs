d8370ac0f093a5f73d951d5b0938ee156e3aed25
ghkgus407 <143527072+ghkgus407@users.noreply.github.com>
2024-05-13 13:38:16 +0900

    Title: 정수 세 개 입력받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b,c;
+	cin >> a >> b;
+    cin >> c;
+
+    cout << fixed;
+    cout.precision(3);
+	cout<<a<<" "<<b<<" "<<c;
+
+	return 0;
+
+}
